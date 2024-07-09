9c48600d30800f4298773a92c314c3c8e0fb2db1
Sumanth <9ssumanth@gmail.com>
2024-07-09 12:33:02 +0530

    bucket name

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "d77-terraform"  #provide bucket name which you have created.
+    bucket = "d77-terraform-1"  #provide bucket name which you have created.
     key    = "aws-ssm-parameters/terraform.tfstate" #key is state file path
     region = "us-east-1"
   }
